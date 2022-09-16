4a949162abaca112f1d759b839536d907ae55c32
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-09-16 19:30:18 -0300

    Requisito 21 ok

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ function submitEmail() {
   });
 }submitEmail();
 
+const checkbox = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
 function agreement() {
-  const checkbox = document.getElementById('agreement');
-  const submitBtn = document.getElementById('submit-btn');
   submitBtn.disabled = !checkbox.checked;
   checkbox.addEventListener('change', () => {
     if (checkbox.checked) {
@@ -38,3 +39,89 @@ function verificar() {
   info.innerHTML = restantes;
 }
 textarea.addEventListener('keyup', verificar);
+
+const nam = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const house = document.querySelector('#house');
+const family = document.querySelectorAll('.div-radio');
+const content = document.querySelectorAll('.subject');
+const rate = document.querySelectorAll('.input-value');
+const obs = document.getElementById('textarea');
+const ul = document.querySelector('ul');
+
+function getFullName() {
+  return `${nam.value} ${lastName.value}`;
+}
+
+function getEmail() {
+  return email.value;
+}
+
+function getHouse() {
+  return house.options[house.selectedIndex].value;
+}
+
+function getFamily() {
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked) {
+      return family[index].value;
+    }
+  }
+}
+
+function getContent() {
+  const valuesContent = [];
+  for (let index = 0; index < content.length; index += 1) {
+    if (content[index].checked) {
+      valuesContent.push(` ${content[index].value}`);
+    }
+  }
+  return valuesContent;
+}
+
+function getRate() {
+  for (let index = 0; index < rate.length; index += 1) {
+    if (rate[index].checked) {
+      return rate[index].value;
+    }
+  }
+}
+
+function getObs() {
+  return obs.value;
+}
+
+const pName = document.createElement('p');
+const pEmail = document.createElement('p');
+const pHouse = document.createElement('p');
+const pFamily = document.createElement('p');
+const pContent = document.createElement('p');
+const pRate = document.createElement('p');
+const pObs = document.createElement('p');
+const li = document.createElement('li');
+
+function newForm() {
+  // e.preventDefault();
+  pName.innerText = `Nome: ${getFullName()}`;
+  pEmail.innerText = `Email: ${getEmail()}`;
+  pHouse.innerText = `Casa: ${getHouse()}`;
+  pFamily.innerText = `Família: ${getFamily()}`;
+  pContent.innerText = `Matérias: ${getContent()}`;
+  pRate.innerText = `Avaliação: ${getRate()}`;
+  pObs.innerText = `Observações: ${getObs()}`;
+
+  li.appendChild(pName);
+  li.appendChild(pEmail);
+  li.appendChild(pHouse);
+  li.appendChild(pFamily);
+  li.appendChild(pContent);
+  li.appendChild(pRate);
+  li.appendChild(pObs);
+  ul.appendChild(li);
+}
+submitBtn.addEventListener('click', () => {
+  newForm();
+  const evaluationForm = document.getElementById('evaluation-form');
+  evaluationForm.style.display = 'none';
+});
